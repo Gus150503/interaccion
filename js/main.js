6948b8464dbe19305809e7f474abb96066112e40
Gus150503 <116202143+Gus150503@users.noreply.github.com>
2022-11-10 14:54:08 -0500

    Commit inicial

---
+++ js/main.js
@@ -0,0 +1,93 @@
+function encender() {
+    var bombilla = document.getElementById('foco');
+    bombilla.src='img/pic_bulbon.gif'; 
+}
+
+function apagar() {
+   var bombilla =document.getElementById('foco');
+   bombilla.src='img/pic_bulboff.gif';
+}
+
+function ocultar(){
+    var bombilla = document.getElementById('foco');
+    bombilla.style.display='none';
+}
+function mostrar(){
+    var bombilla = document.getElementById('foco');
+    bombilla.style.display='';
+}
+
+function cambiarFondo() {
+    var selector = document.getElementById('body');
+    selector.style.backgroundColor="blue";
+}
+
+function seleccionar() {
+    var color = prompt("Ingrese un color en ingles");
+    var selector = document.getElementById('body');
+    selector.style.backgroundColor=color;
+
+}
+
+function cambiarSize() {
+    var selector = document.getElementById('foco');
+    selector.style.cssText="transition: 2s;background-color:black; width: 400px"
+}
+
+
+function circulo(){
+    var figura = document.getElementById('figura')
+    figura.classList.toggle("circulo");
+}
+
+function rombo(){
+    var figura = document.getElementById('figura')
+    figura.classList.toggle("rombo");
+}
+
+function to(){
+    var figura = document.getElementById('figura')
+    figura.classList.toggle("to");
+}
+
+function left(){
+    var figura = document.getElementById('figura')
+    figura.classList.toggle("left");
+}
+
+function bottom(){
+    var figura = document.getElementById('figura')
+    figura.classList.toggle("bottom");
+}
+
+function right(){
+    var figura = document.getElementById('figura')
+    figura.classList.toggle("right")
+}
+
+function diagonaliz(){
+    var figura = document.getElementById('figura')
+    figura.classList.toggle("diagonaliz")
+}
+function diagonalinf(){
+    var figura = document.getElementById('figura')
+    figura.classList.toggle("diagonalinf")
+
+}
+function gif() {
+    document.getElementById('gif').style.display='block';
+
+}
+function triangulo (){
+    var figura = document.getElementById('figura')
+    figura.classList.toggle("triangulo");
+}
+
+function trapecio(){
+    var figura = document.getElementById('figura')
+    figura.classList.toggle("trapecio");
+}
+function luna(){
+    var figura = document.getElementById('figura')
+    figura.classList.toggle("luna");
+}
